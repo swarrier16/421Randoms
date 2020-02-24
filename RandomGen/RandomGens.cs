76e489664c4fc3d62405c0903fa94b943586ae03
swarrier16 <swarrier16@gmail.com>
2020-02-23 22:09:49 -0800

    reupload of working files

---
+++ RandomGen/RandomGens.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace RandomGen
+{
+    public class Randos
+    {
+        public static int Generate(int seed)
+        {
+            var _random = new Random(seed);
+            int _randomNum = _random.Next();
+            return _randomNum;
+        }
+
+        public static int Generate(int seed, int rangeMin, int rangeMax)
+        {
+            var _random = new Random(seed);
+            int _randomNum = _random.Next(rangeMin, rangeMax);
+            return _randomNum;
+        }
+
+        public static int[] Generate(int seed, int size)
+        {
+            var _random = new Random(seed);
+            int[] result = new int [size];
+
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = _random.Next();  
+            }
+
+            return result;
+        }
+
+        public static decimal Generate(decimal seed)
+        {
+            var _random = new Random((int)seed);
+
+            decimal _randomNum = (decimal)_random.NextDouble();
+            _randomNum = (decimal)Math.Round(_randomNum, 1);
+
+            return _randomNum;
+        }
+
+        public static decimal Generate(decimal seed, double rangeMin, double rangeMax)
+        {
+            var _random = new Random((int)seed);
+
+            decimal _randomNum = (decimal)(_random.NextDouble() * (rangeMax - rangeMin) + rangeMin); 
+            _randomNum = (decimal)Math.Round(_randomNum, 2);
+
+            return _randomNum;
+        }
+
+        public static decimal[] Generate(decimal seed, int size)
+        {
+            var _random = new Random((int)seed);
+            decimal _randomNum;
+            decimal[] result = new decimal[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                _randomNum = (decimal)_random.NextDouble();
+                _randomNum = (decimal)Math.Round(_randomNum, 2);
+
+                result[i] = _randomNum;
+            }
+
+            Console.WriteLine(result[4]);
+
+            return result;
+        }
+
+    }
+}
---
+++ RandomTests/RandomFuncTest.cs
@@ -0,0 +1,64 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomGen;
+
+namespace RandomTests
+{   
+    [TestClass]
+    public class RandomTest
+    {
+       
+
+        [TestMethod]
+        public void GenerateDecimalTest()
+        {
+            decimal _value = Randos.Generate(6M);
+
+            Assert.AreEqual(0.9M, _value);
+        }
+
+        [TestMethod]
+        public void DecimalRangeTest()
+        {
+            decimal _value = Randos.Generate(5M, 0, 100);
+
+            Assert.AreEqual(33.84M, _value);
+        }
+
+        [TestMethod]
+        public void IntRangeTest()
+        {
+            int _value = Randos.Generate(5, 0, 100);
+
+            Assert.AreEqual(33, _value);
+        }
+
+        [TestMethod]
+        public void GenerateIntTest()
+        {
+            int _value = Randos.Generate(5);
+
+            Assert.AreEqual(726643700, _value);
+        }
+
+        [TestMethod]
+        public void IntListTest()
+        {
+
+            int[] arrayList = { 726643700, 610783965, 564707973, 1342984399, 995276750 };
+            int[] _value = Randos.Generate(5, 5);
+            CollectionAssert.AreEqual(arrayList , _value);
+        }
+
+        [TestMethod]
+        public void DecimalListTest()
+        {
+
+            decimal[] arrayList = { 0.34M, 0.28M, 0.26M, 0.63M, 0.46M };
+            decimal[] _value = Randos.Generate(5M, 5);
+            CollectionAssert.AreEqual(arrayList, _value);
+        }
+
+
+    }
+}
+  
